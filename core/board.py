019360056f6bd43836673eaa3288bda44d43b0f5
Inkogni7o <ezykov25@gmail.com>
2024-04-15 10:12:56 +0300

    made class of cell

---
+++ core/board.py
@@ -0,0 +1,5 @@
+def get_coords(board):
+    pass
+
+def make_move(board, move, x, y):
+    pass
---
+++ core/cell.py
@@ -0,0 +1,13 @@
+from tkinter import Button
+
+class Cell:
+    def __init__(self, window, id) -> None:
+        self.live = 0
+        self.button = Button(window, text=' ', height=5, width=10)
+        self.id = id
+        self.x, self.y = id % 3, id // 3
+
+    def decrease_life(self) -> None:
+        self.live -= 1
+
+    
