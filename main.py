9af373c30e72d6ef510faa016b461c139c7b3c93
Inkogni7o <ezykov25@gmail.com>
2024-04-15 09:48:48 +0300

    made playing field

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import tkinter as tk
 
 
 window = tk.Tk()
+window.title('New Tic Tac Toe')
+figures = list()
 
+for i in range(9):
+    figures.append(tk.Button(window, text='', width=10, height=5))
+
+for i, button in enumerate(figures):
+    button.grid(row= i % 3, column=i // 3)
 
 window.mainloop()
