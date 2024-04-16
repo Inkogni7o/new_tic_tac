9ec0df8a9b042ca5603cf816949e3abdbef7ee68
Inkogni7o <ezykov25@gmail.com>
2024-04-16 23:09:01 +0300

    add new placement system

--- core/board.py
+++ core/board.py
@@ -8,6 +8,6 @@ def check_move(btns: list, index: int) -> bool:
 def check_win(btns: list) -> bool:
     for i in range(3):
         if ((btns[i * 3].in_cell ==
-            btns[i * 3 + 1].in_cell ==
-            btns[i * 3 + 2].in_cell) and btns[i * 3].in_cell != ''):
+             btns[i * 3 + 1].in_cell ==
+             btns[i * 3 + 2].in_cell) and btns[i * 3].in_cell != ''):
             print(btns[i * 3])
--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from core.cell import Cell
 from core.board import check_move
 
 
+def draw_board():
+    window.geometry('500x500')
+    window.config(bg='black')
+
+
 def move_maid(event):
     abs_coord_x = window.winfo_pointerx() - window.winfo_rootx()
     abs_coord_y = window.winfo_pointery() - window.winfo_rooty()
-    x, y = abs_coord_x // 80, abs_coord_y // 80
+    x, y = (abs_coord_x - 100) // 82, (abs_coord_y - 100) // 86
     index = y * 3 + x
     success = check_move(btns, index)
     if success:
@@ -24,8 +29,10 @@ window = tk.Tk()
 window.title('New Tic Tac Toe')
 btns = [Cell(window) for i in range(9)]
 
+draw_board()
+
 for i, button in enumerate(btns):
-    button.button.grid(row=i // 3, column=i % 3)
+    button.button.place(x=(100 + i % 3 * 82), y=(100 + i // 3 * 86))
 
 window.bind('<Button-1>', move_maid)
 
